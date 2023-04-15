a7b73b852f213e8d0a383411444281adddb596be
henrybewell <zhanghengruic@gmail.com>
2023-04-15 17:34:02 +0800

    :construction: To provide users with more information, other related skills the candidates have and the average score of each skill and the sources of these data

--- candidates/serializers.py
+++ candidates/serializers.py
@@ -1,15 +1,43 @@
+from django.db.models import Q, Avg
 from rest_framework import serializers
 
-from candidates.models import CandidateSkillScores
+from candidates.models import Platform, CandidateSkillScores
+
+
+class PlatformSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Platform
+        fields = ('name',)
+
+    def to_representation(self, instance):
+        return instance.name
 
 
 class CandidateSkillScoresSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=200, source='skill')
+    score = serializers.CharField(max_length=200, )
+    sources = PlatformSerializer(many=True)
+    average = serializers.SerializerMethodField()
+
+    def get_average(self, obj):
+        average = CandidateSkillScores.objects.filter(skill=obj.skill).aggregate(Avg('score'))['score__avg']
+        return average
+
     class Meta:
         model = CandidateSkillScores
-        fields = ["score", "skill"]
+        fields = ['name', 'score', 'sources', 'average']
 
 
-class CandidateSkillSearchSerializer(serializers.Serializer):
-    main_skill_score = serializers.CharField(read_only=True, source='score')
-    candidate_name = serializers.CharField(max_length=200)
-    related_skills = CandidateSkillScoresSerializer(many=True, read_only=True)
+class CandidateSkillSearchSerializer(serializers.ModelSerializer):
+    main_skill_score = serializers.CharField(source='score')
+    candidate_name = serializers.CharField(max_length=200, )
+    related_skills = serializers.SerializerMethodField()
+
+    def get_related_skills(self, obj):
+        queryset = CandidateSkillScores.objects.filter(Q(candidate_name=obj.candidate_name) & ~Q(skill=obj.skill))
+        serializer = CandidateSkillScoresSerializer(queryset, many=True)
+        return serializer.data
+
+    class Meta:
+        model = CandidateSkillScores
+        fields = ('main_skill_score', 'candidate_name', 'related_skills')

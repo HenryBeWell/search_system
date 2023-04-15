549391a2a54c7129b02426562f19871329813a69
henrybewell <zhanghengruic@gmail.com>
2023-04-15 17:32:10 +0800

    :construction: According to the requirement document, add an additional platform table to store the data source platforms.

--- candidates/models.py
+++ candidates/models.py
@@ -12,3 +12,18 @@ class CandidateSkillScores(models.Model):
 
     class Meta:
         db_table = 'candidate_skill_scores'
+        ordering = ['-score']
+
+
+class Platform(models.Model):
+    """
+    Platform Information
+    """
+    name = models.CharField(max_length=255, blank=True, null=True)
+    sources = models.ManyToManyField(CandidateSkillScores, blank=True, related_name='sources')
+
+    class Meta:
+        db_table = 'platform'
+
+    def __str__(self):
+        return self.name
